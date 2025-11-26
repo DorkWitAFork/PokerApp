1ffd457951d37223b7af75b7e08f76c32bf94475
DorkWitAFork <75294127+DorkWitAFork@users.noreply.github.com>
2025-11-25 19:37:08 -0800

    Implement Player class and PlayerDAO

--- src/main/java/database/PlayerDAO.java
+++ src/main/java/database/PlayerDAO.java
@@ -1,4 +1,58 @@
 package database;
+import model.Player;
 
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+// This class will handle all operations concerning the database operations
+// for the Player.
+
+// DB Schema
+/*
+        id INTEGER PRIMARY KEY AUTOINCREMENT
+        name TEXT NOT NULL
+        created_at TEXT NOT NULL
+ */
 public class PlayerDAO {
+    private Connection connection;
+
+    // CONSTRUCTOR
+    public PlayerDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    // INSERT NEW PLAYER INTO DATABASE
+    public Player addPlayer(Player player) throws SQLException {
+        String sql = """
+                INSERT INTO players (name, created_at)
+                VALUES (?, ?)
+                """;
+        // The creation time is going to be the time of adding them.
+        if (player.getCreation() == null) {
+            player.setCreation(LocalDateTime.now());
+        }
+
+        try (PreparedStatement statement =
+                connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, player.getName());
+            statement.setString(2, player.getCreation().toString());
+
+            int rows = statement.executeUpdate();
+            if (rows == 0) {
+                throw new SQLException("Inserting player failed, no rows were updated.");
+            }
+
+            try (ResultSet keys = statement.getGeneratedKeys()) {
+                if (keys.next()) {
+                    player.setID(keys.getInt(1));
+                } else {
+                    throw new SQLException("Inserting player failed, no ID obtained.");
+                }
+            }
+        }
+        return player;
+    }
 }
---
+++ src/main/java/model/Player.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Player {
+    private String name;
+    private int id;
+    private LocalDateTime created_at;
+
+    // An Empty constructor
+    public Player() {}
+
+    // Constructor when making a new player with just the name.
+    public Player(String name) {
+        this.name = name;
+        this.created_at = LocalDateTime.now();
+    }
+
+    // Constructor when reading for DB and passing all parameters.
+    public Player(String name, int id, LocalDateTime created_at) {
+        this.name = name;
+        this.id = id;
+        this.created_at = created_at;
+    }
+
+    // All the setters and getters
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCreation(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getCreation() {
+        return created_at;
+    }
+
+}
