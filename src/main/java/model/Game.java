07a008748689feac6a976bb3d7aecb3aaef9dff6
DorkWitAFork <75294127+DorkWitAFork@users.noreply.github.com>
2025-11-26 07:52:43 -0800

    update DB comment

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -1,4 +1,12 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class Game {
+    private double gameMoneyIn;
+    private double gameMoneyOut;
+    private LocalDateTime start_time;
+    private LocalDateTime end_time;
+    private int id;
+    boolean closed;
 }
