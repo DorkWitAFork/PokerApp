4621244cd855f9ca56d404e3a6e1a9cd35431575
Nick Gomes <cricketclan27@gmail.com>
2025-12-03 16:44:44 -0800

    started the sekeleton

--- src/main/java/ui/PlayerLookupPanel.java
+++ src/main/java/ui/PlayerLookupPanel.java
@@ -1,8 +1,46 @@
 package ui;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PlayerLookupPanel {
+public class PlayerLookupPanel 
+{
+
+    //result panel will hold the resulting lookup in a more contained area, makes it look a bit cleaner
+    private JPanel midPanel, topPanel, bottomPanel, rightPanel, resultPanel;
+    private JFrame thisFrame;
+    private JTextField searchbar;
+    private JButton searchButton;
+
+    public PlayerLookupPanel()
+    {
+        setupElements();
+        setupListen();
+        setupPanels();
+        setupFrame();
+    }
+
+    private void setupFrame()
+    {
+
+    }
+
+    private void setupPanels()
+    {
+
+    }
+
+    private void setupElements()
+    {
+
+    }
+
+    private void setupListen()
+    {
+
+    }
 
 
-    
 
 }
